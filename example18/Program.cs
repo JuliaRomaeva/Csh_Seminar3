abbe0919fc43055d5ed959119daf73e79cc7f0eb
JuliaRomaeva <julia.romaeva@gmail.com>
2023-02-20 20:22:26 +0300

    диапазон координат по номеру четверти добавлено решение преподавателя

--- example18/Program.cs
+++ example18/Program.cs
@@ -17,3 +17,32 @@ if(o == 3){
 if(o == 4){
     Console.WriteLine("X>0; Y<0");
 }
+
+
+// Console.Write("Введите номер четверти: ");
+// int quarter = int.Parse(Console.ReadLine());
+
+// switch (quarter)
+// {
+//     case 1:{
+//         Console.WriteLine("x>0 y>0");
+//         break;
+//         }
+//     case 2:{
+//         Console.WriteLine("x<0 y>0");
+//         break;
+//         }
+//     case 3:{
+//         Console.WriteLine("x<0 y<0");
+//         break;
+//         }
+//     case 4:{
+//         Console.WriteLine("x>0 y<0");
+//         break;
+//         }
+//     default:{
+//         Console.WriteLine("Введена неправильная четверть");
+//         break;
+//         }
+// }
+// Console.WriteLine("Введена неправильная четверть");

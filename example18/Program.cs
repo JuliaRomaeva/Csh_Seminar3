106e6f00c617adf23ffa2188a882473f53d63f3c
JuliaRomaeva <julia.romaeva@gmail.com>
2023-02-20 20:14:57 +0300

    диапазон координат по номеру четверти

---
+++ example18/Program.cs
@@ -0,0 +1,19 @@
+﻿//**Задача 18:** Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+Console.Write("Введите номер четверти оси координат: ");
+int o = int.Parse(Console.ReadLine());
+
+if(o == 1){
+    Console.WriteLine("X>0; Y>0");
+}   
+if(o == 2){
+    Console.WriteLine("X<0; Y>0");
+}
+if(o == 3){
+    Console.WriteLine("X<0; Y<0");
+}
+if(o == 4){
+    Console.WriteLine("X>0; Y<0");
+}
